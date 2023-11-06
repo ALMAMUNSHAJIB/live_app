e3f6cbe258d428d3d99178b949af91be6b225ea5
mamun.euphoria.infotech@gmail.com <mamun.euphoria.infotech@gmail.com>
2023-11-06 21:47:29 +0600

    live stream app

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  MONGODB_USERNAME: "MONGODB_USERNAME",
+  MONGODB_PASSWORD: "MONGODB_PASSWORD",
+  MONGODB_DB_NAME: "MONGODB_DB_NAME",
+
+  //port
+  PORT: process.env.PORT || 5000,
+
+  //secret key for API
+  SECRET_KEY: "SECRET_KEY",
+
+  //secret key for jwt
+  JWT_SECRET: "JWT_SECRET",
+
+  baseURL: "https://www.your_website.com",
+
+  SERVER_KEY: "SERVER_KEY",
+};
