f15e5c94a2625f361ed8e287f520bce70c7cafd5
mamun.euphoria.infotech@gmail.com <mamun.euphoria.infotech@gmail.com>
2023-11-10 11:39:36 +0600

    bug fixing

--- server/userVerify/userVerifyController.js
+++ server/userVerify/userVerifyController.js
@@ -2,12 +2,12 @@ const {UserDivice} = require('./userVerify.model')
 
 exports.verifyUser = async(req, res, next) => {
     try {
-        const { name, imageUrl, deviceSN, googleToken,  } = req.body;
+        const { name, imageUrl, deviceSN, googleToken, googleData } = req.body;
 
         if(name && imageUrl && deviceSN && googleToken){
 
             const divices = new UserDivice({
-                name, imageUrl, deviceSN, googleToken
+                name, imageUrl, deviceSN, googleToken, googleData
             })
             await divices.save();
             return res.status(200).json({
